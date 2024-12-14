a734c7a19604002b49682a2eeb766063843b79a3
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-12-14 16:11:05 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 local gamelist = {
 	[4777817887] = "https://raw.githubusercontent.com/NightHub3384/J./refs/heads/main/bb.lua",
     [1202096104] = "https://raw.githubusercontent.com/NightHub3384/J./refs/heads/main/dve.lua",
-	[5750914919] = "https://raw.githubusercontent.com/NightHub3384/J./refs/heads/main/fh.lua"
+	[5750914919] = "https://api.realaya.xyz/v1/files/l/szwn8f4ttjkb36027rvvgci2nmv8f9px.lua"
 }
 local checkgame = gamelist[game.gameId]
 
